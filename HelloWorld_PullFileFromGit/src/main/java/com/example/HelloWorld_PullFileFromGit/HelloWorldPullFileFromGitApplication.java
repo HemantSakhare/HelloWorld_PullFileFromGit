6d72688e81abd407b5a91a54b3e3958f4f72aa26
HemantSakhare <sakhare.hemant@gmail.com>
2019-05-05 12:57:21 +0530

    Editing test_1

---
+++ HelloWorld_PullFileFromGit/src/main/java/com/example/HelloWorld_PullFileFromGit/HelloWorldPullFileFromGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorld_PullFileFromGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldPullFileFromGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldPullFileFromGitApplication.class, args);
+	}
+
+}
